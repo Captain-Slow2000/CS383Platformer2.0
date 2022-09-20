09377b63a5a571ebdeda1d6fd4a2c5d0fa39a002
sunnydaze99 <tosin.bangudu73@gmail.com>
2022-09-19 20:15:50 -0700

    New Repo Commit

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void MoveToScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID); //loading the scene ID of the scene we want to move to
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("o"))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+    //function called when ever quit button is pressed
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PAP.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Player Animation Parameters
+public class PAP : MonoBehaviour
+{
+
+    public const string moveX = "moveX";
+    public const string isMoving = "isMoving";
+    public const string axisXinput = "Horizontal";
+    public const string forceX = "forceX";
+    public const string impulseY = "impulseY";
+
+
+}
